c46228e283d28b54a77d738a6675967fa1694199
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-16 20:34:06 +0530

    added logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div classNmae="fixed z-5 width-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
-        <img src="" alt="logo" />
+    <div className="fixed z-5 width-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-12 sm:w-44"
+        onClick={() => navigate("/")}
+      />
+      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-blue-600 text-white px-10 py-2.5">Get Started <ArrowRight className="w-4 h-4" /></button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

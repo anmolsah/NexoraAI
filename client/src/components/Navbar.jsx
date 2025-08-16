e9e0aa0bdd180e7ddb42eae16e0cb8cf7aab7035
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-16 20:13:18 +0530

    Add Navbar component and remove unused react.svg file

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div classNmae="fixed z-5 width-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+        <img src="" alt="logo" />
+    </div>
+  )
+}
+
+export default Navbar

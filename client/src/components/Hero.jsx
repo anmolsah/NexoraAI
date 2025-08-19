02cdd60beff4c251d91c356678381191431dc34a
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-19 20:57:43 +0530

    updated the hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,26 +1,17 @@
-
 import React from "react";
 import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
-    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center items-center bg-[url(/background.jpg)] bg-cover bg-no-repeat min-h-screen text-center">
-      <div className="mb-6 max-w-3xl">
-        <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold leading-[1.2]">
-          Create amazing content with
+    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/background.jpg)] bg-cover bg-no-repeat min-h-screen">
+      <div className="text-center mb-6">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]">
+          Create amazing content with{" "}
+          <br />
+          <span className="text-[#173054]">AI Tools</span>
         </h1>
-
-        {/* Logo below the heading */}
-        <div className="mt-6">
-          <img
-            src={assets.logo2}
-            alt="logo"
-            className="w-52 sm:w-64 md:w-72 lg:w-80 2xl:w-[22rem] mx-auto"
-          />
-        </div>
-
-        <p className="mt-6 text-base sm:text-lg md:text-xl">
-          Transform your content creation with our suite of premium AI tools.
+        <p className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gary-700">
+          Transform your content creation with our suite of preium AI tools.
           Write articles, generate images, and enhance workflow.
         </p>
       </div>
@@ -29,30 +20,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-// import React from "react";
-// import { assets } from "../assets/assets";
-
-// const Hero = () => {
-//   return (
-//     <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/background.jpg)] bg-cover bg-no-repeat min-h-screen">
-//       <div className="text-center mb-6">
-//         <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]">
-//           Create amazing content with{" "}
-//           <br />
-//           <img
-//             src={assets.logo2}
-//             alt="logo"
-//             className="w-44 sm:w-52 cursor-pointer text-center"
-//           />
-//         </h1>
-//         <p>
-//           Transform your content creation with our suite of preium AI tools.
-//           Write articles, generate images, and enhance workflow.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;

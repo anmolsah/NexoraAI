5c90c60494ace7d39a6b6b5f4b7ed12df4dd7dc0
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-21 22:45:38 +0530

    completd the sidebar section

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,30 @@
 import { useClerk, useUser } from "@clerk/clerk-react";
+import {
+  Eraser,
+  FileText,
+  Hash,
+  Home,
+  Image,
+  Scissors,
+  SquarePen,
+  User,
+} from "lucide-react";
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+const navItems = [
+  { to: "/ai", label: "Dashboard", Icon: Home },
+  { to: "/ai/write-article", label: "Write Article", Icon: SquarePen },
+  { to: "/ai/blog-titles", label: "Blog Titles", Icon: Hash },
+  { path: "/ai/generate-images", label: "Generate Images", Icon: Image },
+  { path: "/ai/remove-background", label: "Remove Background", Icon: Eraser },
+  { path: "/ai/remove-object", label: "Remove Objects", Icon: Scissors },
+  { path: "/ai/review-resume", label: "Review Resume", Icon: FileText },
+  { path: "/ai/community", label: "Community", Icon: User },
+];
 
 const Sidebar = ({ sidebar, setSidebar }) => {
-  const {user} = useUser();
+  const { user } = useUser();
   const { signOut, openUserProfile } = useClerk();
   return (
     <div
@@ -17,6 +39,52 @@ const Sidebar = ({ sidebar, setSidebar }) => {
           alt="user_avatar"
         />
         <h1 className="mt-1 text-center">{user.fullName}</h1>
+        <div className="px-6 mt-5 text-sm text-gary-600 font-medium">
+          {/* {navItems.map((to, label, Icon) => (
+            <NavLink
+              key={to}
+              to={to}
+              end={to === "/ai"}
+              onClick={() => setSidebar(false)}
+              className={({ isActive }) =>
+                `px-3.5 py-2.5 flex items-center gap-3 rounded ${
+                  isActive
+                    ? "bg-gradient-to-right from-[#3C81F6] TO-[#9234EA] text-white"
+                    : ""
+                }`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <Icon className={`w-4 h-4 ${isActive ? "text-white" : ""}`} />
+                  {label}
+                </>
+              )}
+            </NavLink>
+          ))} */}
+          {navItems.map(({ to, path, label, Icon }, index) => (
+            <NavLink
+              key={index}
+              to={to || path}
+              end={to === "/ai"}
+              onClick={() => setSidebar(false)}
+              className={({ isActive }) =>
+                `px-3.5 py-2.5 flex items-center gap-3 rounded ${
+                  isActive
+                    ? "bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white"
+                    : ""
+                }`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <Icon className={`w-4 h-4 ${isActive ? "text-white" : ""}`} />
+                  {label}
+                </>
+              )}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );

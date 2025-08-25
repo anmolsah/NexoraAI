87ff55f873d4d29318af3c9de76e2900db79b504
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-26 00:55:24 +0530

    feat: add generateBlogTitle and generateImage functions with usage limits and axios integration

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import sql from "./../config/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -35,6 +36,87 @@ export const generateArticle = async (req, res) => {
     });
 
     const content = response.choices[0].message.content;
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt} , ${content}, 'article')`;
+
+    if (plan !== "pro") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: { free_usage: free_usage + 1 },
+      });
+    }
+
+    res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== "pro" && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "Free usage limit exceeded. Please upgrade to pro plan.",
+      });
+    }
+
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt} , ${content}, 'blog-title')`;
+
+    if (plan !== "pro") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: { free_usage: free_usage + 1 },
+      });
+    }
+
+    res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+    const plan = req.plan;
+
+    if (plan !== "pro") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for pro plan users.",
+      });
+    }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+      headers: {
+        "x-api-key": process.env.CLIPDROP_API_KEY,
+      },
+      responseType: "arraybuffer",
+    });
 
     await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt} , ${content}, 'article')`;
 

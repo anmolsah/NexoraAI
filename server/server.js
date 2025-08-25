7f2c8d967d5736a426fbc6c78492e5e2c84677cb
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-25 23:13:18 +0530

    feat: update package dependencies and improve server.js structure

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
-const app =  express();
-
+const app = express();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
-
-app.get("/",(req,res)=>{
-res.send("Server is Live")
+app.get("/", (req, res) => {
+  res.send("Server is Live");
 });
 
-
+app.use(requireAuth());
 
 const PORT = process.env.PORT || 8888;
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});

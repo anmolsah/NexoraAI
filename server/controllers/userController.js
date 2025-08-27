1b38bef714a9d53f1b0caa1bec86ae351edb71d9
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-27 22:35:57 +0530

    feat: implement toggleLikeCreations functionality to like and unlike creations

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,10 +27,29 @@ export const getPublishCreations = async (req, res) => {
 export const toggleLikeCreations = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const {id} = req.body;
-    const creations =
-      await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
-    res.json({ success: true, creations });
+    const { id } = req.body;
+
+    const [creation] = await sql`SELECT * FROM creations WHERE id = ${id}`;
+    if (!creation) {
+      return res.json({ success: false, message: "Creation not found" });
+    }
+
+    const currentLikes = creation.likes;
+    const userIdStr = userId.toString();
+    let updatedLikes;
+    let message;
+
+    if (currentLikes.includes(userIdStr)) {
+      updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+      message = "Creation unliked";
+    } else {
+      updatedLikes = [...currentLikes, userIdStr];
+      message = "Creation liked";
+    }
+
+    const fomattedArray = `{${updatedLikes.json(",")}}`;
+    await sql`UPDATE creations SET likes = ${fomattedArray}::text[] WHERE id = ${id}`;
+    res.json({ success: true, message });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });

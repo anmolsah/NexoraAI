7d59ed3b43d103a78094bc9cfe57c62818758fab
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-27 19:13:48 +0530

    feat: add removeImageBackground function for background removal and multer configuration

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -140,3 +140,34 @@ export const generateImage = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const removeImageBackground = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { image } = req.file;
+    const plan = req.plan;
+
+    if (plan !== "pro") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for pro plan users.",
+      });
+    }
+
+    const { secure_url } = await cloudinary.uploader.upload(image.path, {
+      transformation: [
+        {
+          effect: "background_removal",
+          background_removal: "remove_the_background",
+        },
+      ],
+    });
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Remove background from image' , ${secure_url}, 'image')`;
+
+    res.json({ success: true, content: secure_url });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
